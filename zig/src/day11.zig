8a20bc52d2d7216d997ada93463ec342b3706fd4
Andrew Reisdorph <andrew_reisdorph@bio-rad.com>
2024-12-11 20:48:00 -0700

    day11 optimized

--- zig/src/day11.zig
+++ zig/src/day11.zig
@@ -6,39 +6,66 @@ const day = 11;
 const data_path = std.fmt.comptimePrint("../data/day{d}.txt", .{day});
 const small_data_path = std.fmt.comptimePrint("../data/day{d}_small.txt", .{day});
 
-const StoneMap = std.AutoHashMap(u64, std.AutoHashMap(u32, u64));
+fn StoneMap(blinks: comptime_int) type {
+    return struct {
+        const Self = @This();
+        const BlinkLookup = [blinks + 1]u64;
+        inner: std.AutoHashMap(u64, BlinkLookup),
 
-fn get_stone_count_after_n_blinks(stone_number: u64, blinks: u8, count_map: *StoneMap) u64 {
+        pub fn init(allocator: std.mem.Allocator) Self {
+            return Self{ .inner = std.AutoHashMap(u64, BlinkLookup).init(allocator) };
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.inner.deinit();
+        }
+
+        pub fn get(self: *Self, key: u64, num_blinks: u32) u64 {
+            if (self.inner.get(key)) |entry| {
+                return entry[num_blinks];
+            }
+            return 0;
+        }
+
+        pub fn set(self: *Self, key: u64, num_blinks: u32, stone_count: u64) void {
+            var result = self.inner.getOrPut(key) catch unreachable;
+            if (result.found_existing) {
+                result.value_ptr[num_blinks] = stone_count;
+            } else {
+                result.value_ptr.* = std.mem.zeroes(BlinkLookup);
+                result.value_ptr[num_blinks] = stone_count;
+            }
+        }
+    };
+}
+
+fn get_stone_count_after_n_blinks(stone_number: u64, blinks: u8, stone_map: anytype) u64 {
     // Base Case
     if (blinks == 0) {
         return 1;
     }
 
-    if (count_map.get(stone_number)) |map_for_this_stone| {
-        if (map_for_this_stone.get(blinks)) |count| {
-            return count;
-        }
-    } else {
-        count_map.put(stone_number, std.AutoHashMap(u32, u64).init(std.heap.page_allocator)) catch @panic("Could not create map");
+    const lookup = stone_map.get(stone_number, blinks);
+    if (lookup > 0) {
+        return lookup;
     }
 
     var stone_count: u64 = 0;
-
     if (stone_number == 0) {
-        stone_count = get_stone_count_after_n_blinks(1, blinks - 1, count_map);
+        stone_count = get_stone_count_after_n_blinks(1, blinks - 1, stone_map);
     } else {
         const stone_number_log10: u32 = @as(u32, @intFromFloat(@floor(std.math.log10(@as(f64, @floatFromInt(stone_number))))));
         if (stone_number_log10 % 2 == 1) {
             const divisor = std.math.pow(u32, 10, @intFromFloat(@ceil(@as(f32, @floatFromInt(stone_number_log10)) / 2.0)));
             const left = stone_number / divisor;
             const right = stone_number % divisor;
-            stone_count = get_stone_count_after_n_blinks(left, blinks - 1, count_map) + get_stone_count_after_n_blinks(right, blinks - 1, count_map);
+            stone_count = get_stone_count_after_n_blinks(left, blinks - 1, stone_map) + get_stone_count_after_n_blinks(right, blinks - 1, stone_map);
         } else {
-            stone_count = get_stone_count_after_n_blinks(stone_number * 2024, blinks - 1, count_map);
+            stone_count = get_stone_count_after_n_blinks(stone_number * 2024, blinks - 1, stone_map);
         }
     }
 
-    count_map.getPtr(stone_number).?.put(blinks, stone_count) catch @panic("Could not create map");
+    stone_map.set(stone_number, blinks, stone_count);
 
     return stone_count;
 }
@@ -46,17 +73,14 @@ fn get_stone_count_after_n_blinks(stone_number: u64, blinks: u8, count_map: *Sto
 pub fn part_one(reader: *Reader) u64 {
     var sum: u64 = 0;
 
-    var map = StoneMap.init(std.heap.page_allocator);
+    const blinks = 25;
+    var map = StoneMap(blinks).init(std.heap.page_allocator);
+    defer map.deinit();
+
     while (reader.next_int(u64, false)) |stone_number| {
         _ = reader.next_char();
-        sum += get_stone_count_after_n_blinks(stone_number, 25, &map);
+        sum += get_stone_count_after_n_blinks(stone_number, blinks, &map);
     }
-
-    var map_iter = map.iterator();
-    while (map_iter.next()) |entry| {
-        entry.value_ptr.deinit();
-    }
-    map.deinit();
 
     return sum;
 }
@@ -64,19 +88,13 @@ pub fn part_one(reader: *Reader) u64 {
 pub fn part_two(reader: *Reader) u64 {
     var sum: u64 = 0;
 
-    var map = StoneMap.init(std.heap.page_allocator);
+    const blinks = 75;
+    var map = StoneMap(blinks).init(std.heap.page_allocator);
+    defer map.deinit();
     while (reader.next_int(u64, false)) |stone_number| {
         _ = reader.next_char();
-        sum += get_stone_count_after_n_blinks(stone_number, 75, &map);
+        sum += get_stone_count_after_n_blinks(stone_number, blinks, &map);
     }
-
-    var map_iter = map.iterator();
-    var max: u64 = 0;
-    while (map_iter.next()) |entry| {
-        entry.value_ptr.deinit();
-        max = @max(max, entry.key_ptr.*);
-    }
-    map.deinit();
 
     return sum;
 }
