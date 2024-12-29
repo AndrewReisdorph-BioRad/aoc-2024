2c69cda28b6bb5358e5af6f202de5e8e20639598
Andrew Reisdorph <andrew_reisdorph@bio-rad.com>
2024-12-29 11:34:11 -0700

    day22: part 2

--- zig/src/day22.zig
+++ zig/src/day22.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Reader = @import("utils/reader.zig").Reader;
 const benchmark = @import("utils/benchmark.zig");
+const BitField = @import("utils/bitfield.zig").Bitfield;
 
 const day = 22;
 const data_path = std.fmt.comptimePrint("../data/day{d}.txt", .{day});
@@ -20,27 +21,29 @@ pub fn part_one(reader: *Reader) u64 {
     return sum;
 }
 
-const Node = struct { children: [19]?*Node = .{null} ** 19, count: u64 = 0 };
-
 const Graph = struct {
     const Self = @This();
-    root: Node = Node{},
-    max_bananas: u64 = 0,
-    fn insert(self: *Self, address: [4]i8, price: u8, allocator: std.mem.Allocator) void {
-        var current = &self.root;
-        for (0..address.len) |idx| {
-            const child_idx = @as(usize, @intCast(address[idx] + 9));
-            if (current.children[child_idx] == null) {
-                current.children[child_idx] = allocator.create(Node) catch unreachable;
-                current.children[child_idx].?.* = Node{};
-            }
-            current = current.children[child_idx].?;
-        }
-        current.count += price;
-        // if (address[0] == -2 and address[1] == 1 and address[2] == -1) {
-        //     std.debug.print("{any} -- {d} -- {}\n", .{ address, price, current.count });
-        // }
-        self.max_bananas = @max(self.max_bananas, current.count);
+    nodes: [19][19][19][19]u16 = std.mem.zeroes([19][19][19][19]u16),
+    max: u16 = 0,
+    fn insert(self: *Self, address: [4]i8, price: u8) void {
+        const node = &self.nodes[@as(usize, @intCast(address[0] + 9))][@as(usize, @intCast(address[1] + 9))][@as(usize, @intCast(address[2] + 9))][@as(usize, @intCast(address[3] + 9))];
+        node.* += price;
+        self.max = @max(self.max, node.*);
+    }
+};
+
+const DeltaSet = struct {
+    const Self = @This();
+    // For some reason using 19][19][19][19]u1 here causes the compiler to hang, but tests run just fine
+    seen: [19][19][19][19]u8 = std.mem.zeroes([19][19][19][19]u8),
+    fn set(self: *Self, address: [4]i8) void {
+        self.seen[@as(usize, @intCast(address[0] + 9))][@as(usize, @intCast(address[1] + 9))][@as(usize, @intCast(address[2] + 9))][@as(usize, @intCast(address[3] + 9))] = 1;
+    }
+    fn get(self: *Self, address: [4]i8) bool {
+        return self.seen[@as(usize, @intCast(address[0] + 9))][@as(usize, @intCast(address[1] + 9))][@as(usize, @intCast(address[2] + 9))][@as(usize, @intCast(address[3] + 9))] == 1;
+    }
+    fn reset(self: *Self) void {
+        self.seen = std.mem.zeroes(@TypeOf(self.seen));
     }
 };
 
@@ -48,15 +51,15 @@ const CircularArray = struct {
     const Self = @This();
     data: [4]i8 = undefined,
     len: usize = 0,
-    insert: usize = 0,
+    write: usize = 0,
     read: usize = 0,
 
     fn push(self: *Self, value: i8) void {
-        if (self.insert >= self.data.len) {
-            self.insert = 0;
+        if (self.write >= self.data.len) {
+            self.write = 0;
         }
-        self.data[self.insert] = value;
-        self.insert += 1;
+        self.data[self.write] = value;
+        self.write += 1;
         if (self.len == self.data.len) {
             if (self.read == self.data.len - 1) {
                 self.read = 0;
@@ -67,7 +70,7 @@ const CircularArray = struct {
         self.len = @min(self.data.len, self.len + 1);
     }
 
-    fn get(self: *Self) [4]i8 {
+    fn get(self: *Self) @TypeOf(self.data) {
         var buffer: @TypeOf(self.data) = undefined;
         for (0..self.data.len) |i| {
             const adjusted_idx = (self.read + i) % self.data.len;
@@ -76,53 +79,70 @@ const CircularArray = struct {
         return buffer;
     }
 
+    fn full(self: *Self) bool {
+        return self.len == self.data.len;
+    }
+
+    fn reset(self: *Self) void {
+        @memset(&self.data, 0);
+        self.len = 0;
+        self.read = 0;
+        self.write = 0;
+    }
+
     fn data_as_u32(self: *Self) u32 {
         const buffer = self.get();
         return std.mem.readInt(u32, @ptrCast(&buffer), std.builtin.Endian.little);
     }
 };
 
+const Secret = struct {
+    const Self = @This();
+    inner: u64 = 0,
+    pub fn cycle(self: *Self) void {
+        self.inner = ((self.inner * 64) ^ self.inner) & 0xffffff;
+        self.inner = ((self.inner / 32) ^ self.inner) & 0xffffff;
+        self.inner = ((self.inner * 2048) ^ self.inner) & 0xffffff;
+    }
+    pub fn price(self: *Self) u8 {
+        return @as(u8, @intCast(self.inner % 10));
+    }
+};
+
 pub fn part_two(reader: *Reader) u64 {
-    const allocator = std.heap.page_allocator;
+    const iterations = 2000;
     var graph: Graph = Graph{};
-    var buffer = CircularArray{};
-    var prices_for_this_seller = std.AutoHashMap(u32, void).init(allocator);
-    var last_price: ?u8 = null;
+    var circular_buffer = CircularArray{};
+    var prices_for_this_seller = DeltaSet{};
+    var last_price: u8 = undefined;
+    var secret = Secret{};
 
     while (reader.search_next_int(u64)) |initial| {
-        var secret = initial;
+        secret.inner = initial;
+        prices_for_this_seller.reset();
+        circular_buffer.reset();
+        last_price = secret.price();
 
-        prices_for_this_seller.clearRetainingCapacity();
+        for (iterations) |_| {
+            secret.cycle();
+            const price = secret.price();
 
-        for (0..2000) |_| {
-            secret = ((secret * 64) ^ secret) & 0xffffff;
-            secret = ((secret / 32) ^ secret) & 0xffffff;
-            secret = ((secret * 2048) ^ secret) & 0xffffff;
+            const delta = @as(i8, @intCast(last_price)) - @as(i8, @intCast(price));
+            circular_buffer.push(delta);
 
-            const price = @as(u8, @intCast(secret % 10));
-            // std.debug.print("price: {d} secret: {d} ", .{ price, secret });
-
-            if (last_price) |p| {
-                const delta = @as(i8, @intCast(p)) - @as(i8, @intCast(price));
-                buffer.push(delta);
-                // std.debug.print("delta: {d} deltas: {any}\n", .{ delta, buffer.get() });
-
-                if (buffer.len == 4) {
-                    const key = buffer.data_as_u32();
-                    if (prices_for_this_seller.get(key) == null) {
-                        graph.insert(buffer.get(), price, allocator);
-                        prices_for_this_seller.put(key, {}) catch unreachable;
-                    }
+            if (circular_buffer.full()) {
+                const deltas = circular_buffer.get();
+                if (!prices_for_this_seller.get(deltas)) {
+                    graph.insert(circular_buffer.get(), price);
+                    prices_for_this_seller.set(deltas);
                 }
-            } else {
-                // std.debug.print("\n", .{});
             }
 
             last_price = price;
         }
     }
 
-    return graph.max_bananas;
+    return @as(u64, graph.max);
 }
 
 pub fn do_benchmark() void {
@@ -171,6 +191,5 @@ test "part 2 sample" {
 test "part 2 big" {
     var reader = Reader.from_comptime_path(data_path);
     const result = part_two(&reader);
-    // 2236 is too low
-    try std.testing.expectEqual(1, result);
+    try std.testing.expectEqual(2242, result);
 }
